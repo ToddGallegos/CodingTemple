34e77e5f32c95772a926131f2ad3f51885c792ec
ToddGallegos <92834462+ToddGallegos@users.noreply.github.com>
2022-11-24 04:17:27 -0800

    Update prework.py

--- prework.py
+++ prework.py
@@ -4,7 +4,7 @@
 # Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
 
 def hello_name(user_name):
-    print("hello_" + user_name + "!")
+    print("hello_" + user_name.upper() + "!")
     return
                 
 # Question 2
