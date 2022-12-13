16e06dc1fc5abe37ed607f957326f7b87e8dca03
ToddGallegos <toroga@hotmail.com>
2022-12-12 17:10:51 -0800

    updated q5 to start at 0 index

--- prework.py
+++ prework.py
@@ -45,7 +45,7 @@ def is_leap_year(a_year):
 # Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
 
 def is_consecutive(a_list):
-    for num in range(1, len(a_list) - 1):
+    for num in range(0, len(a_list) - 1):
         if a_list[num+1] != a_list[num] + 1:
             return False
     return True
