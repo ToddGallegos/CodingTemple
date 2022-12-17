2d8d31306fce54844f7f56b3fa6f2898968be9b7
ToddGallegos <toroga@hotmail.com>
2022-12-17 02:21:18 -0800

    update

--- prework.py
+++ prework.py
@@ -1,4 +1,4 @@
-# Todd Gallegos - Coding Temple
+# Todd G - Coding Temple
 
 # Question 1
 # Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
