075574a0cf8c07cf76a99f4a7d496b7b8146cf3c
ToddGallegos <toroga@hotmail.com>
2023-01-07 03:56:50 -0800

    changed 3 variables into single dict

--- week_3_project/roi_calculator.py
+++ week_3_project/roi_calculator.py
@@ -6,13 +6,11 @@ from roi_helper_functions import get_income, get_expenses, get_investment, get_r
 
 def roi_calc():
     running = True
-    total_income = 0
-    total_expenses = 0
-    initial_investment = 0
+    values = {"total_income": 0, "total_expenses": 0, "initial_investment": 0}
     while running == True:
         print('*'*50,"\n*   Welcome to the Cash-On-Cash ROI Calculator   *")
         print('*'*50,"\n")
-        print(f"Current Cash-On-Cash ROI = {round(get_roi(total_income, total_expenses, initial_investment),2)}%\nCurrent Monthly Rental Income: ${total_income:,}\nCurrent Monthly Rental Expenses: ${total_expenses:,}\nCurrent Initial Investment: ${initial_investment:,}\n")
+        print(f"Current Cash-On-Cash ROI = {round(get_roi(values['total_income'], values['total_expenses'], values['initial_investment']),2)}%\nCurrent Monthly Rental Income: ${values['total_income']:,}\nCurrent Monthly Rental Expenses: ${values['total_expenses']:,}\nCurrent Initial Investment: ${values['initial_investment']:,}\n")
         print("Please enter all applicable data below using only numbers. Blank fields will default to 0.\n\nPLEASE CHOOSE:\n0) Quit\n1) Change Monthly Rental Income\n2) Change Monthly Rental Expenses\n3) Change Initial Investment\n4) Clear Current Values\n")
         try:    
             choice = int(input("CHOICE NUMBER: "))
@@ -20,13 +18,13 @@ def roi_calc():
                 case 0:
                     running = False
                 case 1:
-                    total_income = get_income()
+                    values['total_income'] = get_income()
                 case 2:
-                    total_expenses = get_expenses()
+                    values['total_expenses'] = get_expenses()
                 case 3:
-                    initial_investment = get_investment()
+                    values['initial_investment'] = get_investment()
                 case 4:
-                    total_income, total_expenses, initial_investment = 0,0,0
+                    values['total_income'], values['total_expenses'], values['initial_investment'] = 0,0,0
                 case _:
                     input("I'm not even sure how you got to this line of code. It doesn't even need to exist really.")
         except:
