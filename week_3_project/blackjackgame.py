8ff76785eb811817b4fa8335621df9d092d15c79
ToddGallegos <toroga@hotmail.com>
2023-01-08 22:07:47 -0800

    testing data structures

--- week_3_project/blackjackgame.py
+++ week_3_project/blackjackgame.py
@@ -1,8 +1,8 @@
 import random
 
-deck = {2:4, 3:4, 4:4, 5:4, 6:4, 7:4, 8:4, 9:4, 10:4, 11:4, 12:4, 13:4, 14:4}
 
-def get_card():
+
+def get_card(deck):
     num1 = random.randint(2, 14)
     if num1 < 11 and deck[num1] > 0:
         mycard = num1
@@ -23,10 +23,39 @@ def get_card():
         mycard = get_card()
     return mycard 
     
-mycard1 = get_card()
-mycard2 = get_card()
-mycard3 = get_card()
-mycard4 = get_card()
-
+def play_blackjack():
+    deck = {2:4, 3:4, 4:4, 5:4, 6:4, 7:4, 8:4, 9:4, 10:4, 11:4, 12:4, 13:4, 14:4}
+    shadow_deck = deck
+    player_value = 0
+    cpu_value = 0
     
-print(mycard1, mycard2, mycard3, mycard4, deck)
+    player_cards = [get_card(deck), get_card(deck)]
+    cpu_cards = [get_card(deck), get_card(deck)]
+    
+    for card in player_cards:
+        if card < 11:
+            deck[card] -= 1
+        elif card == "J":
+            deck[11] -= 1
+        elif card == "Q":
+            deck[12] -= 1
+        elif card == "K":
+            deck[13] -= 1
+        elif card == "A":
+            deck[14] -= 1
+            
+    for card in cpu_cards:
+        if card < 11:
+            deck[card] -= 1
+        elif card == "J":
+            deck[11] -= 1
+        elif card == "Q":
+            deck[12] -= 1
+        elif card == "K":
+            deck[13] -= 1
+        elif card == "A":
+            deck[14] -= 1
+    
+    print(player_cards, cpu_cards)
+    
+play_blackjack()
