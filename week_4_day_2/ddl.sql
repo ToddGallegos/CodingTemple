35f1717bddc68fd833f31185479716400094c2c6
ToddGallegos <toroga@hotmail.com>
2023-01-11 04:09:40 -0800

    deleted practice file

--- week_4_day_2/ddl.sql
+++
@@ -1,9 +0,0 @@
--- create customer table
-CREATE TABLE customer(
-    customer_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    age INTEGER,
-    email VARCHAR(150)
-);
-
