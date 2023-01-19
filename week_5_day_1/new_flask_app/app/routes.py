8a735cd606373212e4b5b5a8a000a8cb7524bc21
ToddGallegos <toroga@hotmail.com>
2023-01-19 01:40:11 -0800

    incorporate api call. capture and display data

--- week_5_day_1/new_flask_app/app/routes.py
+++ week_5_day_1/new_flask_app/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from .forms import PokemonCatcherForm
+import requests
 
 @app.route('/')
 def homepage():
@@ -9,4 +10,25 @@ def homepage():
 @app.route('/pokemon', methods=["GET", "POST"])
 def pokemon():
     form = PokemonCatcherForm()
+    if request.method == "POST":
+        if form.validate():
+            pokemon_name = form.pokemon_name.data
+            
+            def pokemon_info(p_name):
+                response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{p_name.lower()}')
+                
+                if response.ok:
+                    my_pokemon = {'pokemon_name': response.json()['forms'][0]['name'], 
+                                'base_hp': response.json()['stats'][0]['base_stat'], 
+                                'base_attack': response.json()['stats'][1]['base_stat'], 
+                                'base_defense': response.json()['stats'][2]['base_stat'], 
+                                'base_experience': response.json()['base_experience'], 
+                                'ability_name': response.json()['abilities'][0]['ability']['name'], 
+                                'front_shiny_sprite': response.json()['sprites']['front_shiny']}
+                    return my_pokemon
+
+            my_pokemon = pokemon_info(pokemon_name)
+            if my_pokemon:
+                return render_template('pokemon.html', form = form, my_pokemon = my_pokemon)
+            
     return render_template('pokemon.html', form = form)
