ae8a448b79a05d7d973f62420d46bd48caa105b0
ToddGallegos <toroga@hotmail.com>
2023-01-23 06:04:21 -0800

    workarounds work

--- week_5_day_1/new_flask_app/config.py
+++ week_5_day_1/new_flask_app/config.py
@@ -4,7 +4,10 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 
 class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
+    print(os.environ.get('FLASK_ENV'))
     FLASK_ENV = os.environ.get('FLASK_ENV')
+    print(os.environ.get('SECRET_KEY'))
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    print(os.environ.get('DATABASE_URL'))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- week_5_day_1/new_flask_app/run.py
+++ week_5_day_1/new_flask_app/run.py
@@ -1,3 +1,5 @@
+# from dotenv import load_dotenv
+# load_dotenv()
 from app import app
 
 if __name__ == "__main__":
